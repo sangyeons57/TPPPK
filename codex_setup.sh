57fa3bf29b76c9ac2c487cf4121d9b9a1c1d86c9
sangyeons57 <sangyeons57@gmail.com>
2025-07-13 03:20:36 +0000

    보기좋게만들기

--- codex_setup.sh
+++ codex_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "Install openjdk-17"
+echo "==========Install openjdk-17==========="
 apt-get update
 apt-get install -y openjdk-17-jdk
 
+echo "==========SET JAVA_HOME openjdk-17==========="
 update-java-alternatives --list
 update-java-alternatives --set java-1.17.0-openjdk-amd64
 echo $JAVA_HOME
@@ -12,17 +13,18 @@ java -version
 export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
-echo "Getting Android Studio"
+echo "==========Install Android SDK==========="
 wget -O android-commandlinetools.zip https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip
 
-echo "Unpacking Android Studio"
+
+echo "==========Unpacking Android Studio=========="
 unzip android-commandlinetools.zip -d /usr/lib/android-sdk
 
-echo "Updating sdkmanager"
+echo "==========Updating sdkmanager=========="
 /usr/lib/android-sdk/cmdline-tools/bin/sdkmanager --sdk_root=/usr/lib/android-sdk/ --update
 
-echo "Installing Android SDK"
+echo "==========Installing Android SDK=========="
 bash -c 'yes | /usr/lib/android-sdk/cmdline-tools/bin/sdkmanager --sdk_root=/usr/lib/android-sdk/ "platforms;android-35"'
 
-echo "Git Submodule Init"
+echo "==========Git Submodule Init=========="
 git submodule update --init --recursive
